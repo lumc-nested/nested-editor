1481047a41bffc26a60768dfb135fcd587fc8d56
Z. Tatum <z.tatum@lumc.nl>
2014-12-15 16:35:00 +0100

    Use React TestUtil

--- test/spec/components/PedigreeApp.js
+++ test/spec/components/PedigreeApp.js
@@ -1,19 +1,19 @@
 'use strict';
 
 describe('Main', function () {
-  var React, PedigreeApp, component;
+  var React, TestUtils, PedigreeApp, component;
 
   beforeEach(function () {
-    var container = document.createElement('div');
-    container.id = 'content';
-    document.body.appendChild(container);
-
-    React = require('react');
+    React = require('react/addons');
+    TestUtils = React.addons.TestUtils;
     PedigreeApp = React.createFactory(require('../../../src/scripts/components/PedigreeApp.jsx'));
     component = PedigreeApp();
   });
 
-  it('should create a new instance of PedigreeApp', function () {
+  it('should create a new instance of PedigreeApp', function() {
     expect(component).toBeDefined();
+    expect(TestUtils.isElement(component)).toBe(true);
+    expect(TestUtils.isElementOfType(component, PedigreeApp)).toBe(true);
   });
+
 });
