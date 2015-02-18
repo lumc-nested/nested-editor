8b3b9c98d01e98390fd297bd1eb8a4ddc4af9894
Martijn Vermaat <martijn@vermaat.name>
2015-02-19 00:47:46 +0100

    Fix webpack dist build by including web font loaders

--- webpack.dist.config.js
+++ webpack.dist.config.js
@@ -70,6 +70,18 @@ module.exports = {
     }, {
       test: /\.pegjs$/,
       loader: 'pegjs-loader'
+    }, {
+      test: /\.woff$/,
+      loader: "url-loader?limit=10000&minetype=application/font-woff"
+    }, {
+      test: /\.ttf$/,
+      loader: "file-loader"
+    }, {
+      test: /\.eot$/,
+      loader: "file-loader"
+    }, {
+      test: /\.svg$/,
+      loader: "file-loader"
     }]
   }
 };
