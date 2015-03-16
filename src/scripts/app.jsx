58c4cba12e16dc660d0fd81ea5cf51ceae4e77e7
Martijn Vermaat <martijn@vermaat.name>
2015-03-16 15:11:04 +0100

    Update ESLint dependency to 0.17.1
    We now need `eslint-plugin-react` to properly lint JSX files, see:
    
    http://eslint.org/blog/2015/03/eslint-0.17.0-released/#changes-to-jsx/react-handling

--- src/scripts/app.jsx
+++ src/scripts/app.jsx
@@ -1,6 +1,8 @@
 'use strict';
 
 
+// TODO: I hope this is a temporary workaround for an ESLint bug?
+/*eslint-disable no-unused-vars, no-undef */
 var React = require('react');
 var PedigreeApp = require('./components/PedigreeApp');
 
